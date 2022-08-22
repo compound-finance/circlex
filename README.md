f03dd5245b8fc442b35c70fdf4e6e18d29258b3f
Aryan Bhasin <bhasinaryan@gmail.com>
2022-08-22 18:13:01 -0400

    Add VAN to mock wire API endpoint (#6)

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ by adding `circlex_api` to your list of dependencies in `mix.exs`:
 ```elixir
 def deps do
   [
-    {:circlex_api, "~> 0.1.8"}
+    {:circlex_api, "~> 0.1.9"}
   ]
 end
 ```
--- lib/circlex/api/payments/payments.ex
+++ lib/circlex/api/payments/payments.ex
@@ -102,21 +102,25 @@ defmodule Circlex.Api.Payments.Payments do
 
       iex> host = Circlex.Test.start_server()
       iex> amount = %{amount: "12345.00", currency: "USD"}
-      iex> Circlex.Api.Payments.Payments.mock_wire("CIR3KX3L99", amount, host: host)
+      iex> Circlex.Api.Payments.Payments.mock_wire("CIR3KX3L99", amount, "1000000001", host: host)
       {:ok,
        %{
          "amount" => %{"amount" => "12345.00", "currency" => "USD"},
          "status" => "pending",
-         "trackingRef" => "CIR3KX3L99"
+         "trackingRef" => "CIR3KX3L99",
+         "beneficiaryBank" => %{"accountNumber" => "1000000001"}
        }}
   """
-  def mock_wire(tracking_ref, amount, opts \\ []) do
+  def mock_wire(tracking_ref, amount, account_number, opts \\ []) do
     with {:ok, res} <-
            api_post(
              "/v1/mocks/payments/wire",
              %{
                trackingRef: tracking_ref,
-               amount: amount
+               amount: amount,
+               beneficiaryBank: %{
+                accountNumber: account_number
+               }
              },
              opts
            ) do
